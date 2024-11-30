d50b90b67f734475517ba7ef5e1feaa718c158b3
May H <mayh500@gmail.com>
2024-11-30 21:46:10 +0200

    changing title

--- Assets/UpdateScore.cs
+++ Assets/UpdateScore.cs
@@ -2,17 +2,36 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using System.Threading.Tasks;
 
 public class UpdateScore : MonoBehaviour
 {
     public TextMeshProUGUI gameOutcome;
     public TextMeshProUGUI xScoreText;
     public TextMeshProUGUI oScoreText;
+    public TextMeshProUGUI updateTitle;
     public ScriptableGameData ScriptableData;
 
     private void Awake()
     {
         ScriptableData.UpdateGameOutcome(gameOutcome, xScoreText, oScoreText);
+    }
+
+    private async void OnEnable()
+    {
+        await Task.Delay(10);
+        if (ScriptableData.curentGameType == GameType.LocalPVP)
+        {
+            updateTitle.text = "Local PvP";
+        }
+        if (ScriptableData.curentGameType == GameType.VSComp)
+        {
+            updateTitle.text = "Player VS Computer";
+        }
+        if (ScriptableData.curentGameType == GameType.VSAI)
+        {
+            updateTitle.text = "Player VS AI";
+        }
     }
 
     void Update()
